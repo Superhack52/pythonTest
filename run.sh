6ee44c4831530637ac9fe1514a87aad24b05024b
Сергей Красильников <ss.krasilnikov@jet.su>
2021-08-24 14:19:04 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 function generate_json {
 cat <<EOF
-"{
+{
 	"buildType": {
     	"id": "PythonTest_GetResult"
 	}
-}"
+}
 EOF
 }
 
@@ -13,5 +13,4 @@ echo Test begin
 pytest ./main.py --alluredir=./allure_results
 echo Test ended
 
-curl -v --header "Authorization: Bearer ${TOKEN}" --request POST "http://teamcity-server-instance:8111/app/rest/buildQueue/" --data --data "$(generate_json)" --header "Content-Type: application/json"
-
+curl -v --header "Authorization: Bearer ${TOKEN}" --request POST "http://localhost:8111/app/rest/buildQueue/" --data "$(generate_json)" --header "Content-Type: application/json"
