5e79a8f6fad3310fa3d304cb6ea3dda2a06b674f
Сергей Красильников <ss.krasilnikov@jet.su>
2021-08-24 13:51:09 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
+function generate_json {
+cat <<EOF
+{
+	"buildType": {
+    	"id": "PythonTest_GetResult"
+	},
+	"properties": {
+    	"property": [{
+            	"name": "env.container_name",
+            	"value": ${CONTAINER_NAME}
+     	   }
+    	]
+	}
+}
+EOF
+}
+
 echo Test begin
 pytest ./main.py --alluredir=./allure_results
 echo Test ended
-curl -v --header "Authorization: Bearer ${TOKEN}" --request POST "http://teamcity-server-instance:8111/app/rest/buildQueue/" --data "{ 'buildType': { 'id': 'PythonTest_GetResultContainer' }, 'properties': { 'property': [{ 'name': 'env.container_name', 'value': ${CONTAINER_NAME} } ]}}" --header "Content-Type: application/json"
+
+curl -v --header "Authorization: Bearer ${TOKEN}" --request POST "http://teamcity-server-instance:8111/app/rest/buildQueue/" --data --data "$(generate_json)" --header "Content-Type: application/json"
+
